f99c51843de6039700380c16f6400dd54692430a
Aditya Orade <adityaraoorade@gmail.com>
2025-10-09 11:26:18 +0530

    normal updates

--- grade_Percentage.c
+++ grade_Percentage.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
-
 int main() {
     float physics, chemistry, biology, mathematics, computer;
     float total, percentage;
-
-    printf("Enter marks for Physics: ");
+    printf("Enter marks of Physics: ");
     scanf("%f", &physics);
-    printf("Enter marks for Chemistry: ");
+    printf("Enter marks of Chemistry: ");
     scanf("%f", &chemistry);
-    printf("Enter marks for Biology: ");
+    printf("Enter marks of Biology: ");
     scanf("%f", &biology);
-    printf("Enter marks for Mathematics: ");
+    printf("Enter marks of Mathematics: ");
     scanf("%f", &mathematics);
-    printf("Enter marks for Computer: ");
+    printf("Enter marks of Computer: ");
     scanf("%f", &computer);
 
-
-    total = physics + chemistry + biology + mathematics + computer;
+  
+total = physics + chemistry + biology + mathematics + computer;
     percentage = (total / 500) * 100;
 
-
+  
     printf("\nTotal Marks = %.2f\n", total);
-    printf("Percentage = %.2f%%\n", percentage);
+    printf("Percentage = %.2f\n", percentage);
 
-    if (percentage >= 90)
+  
+    if (percentage >= 90) {
         printf("Grade: A\n");
-    else if (percentage >= 80)
+    }
+    else if (percentage >= 80) {
         printf("Grade: B\n");
-    else if (percentage >= 70)
+    }
+    else if (percentage >= 70) {
         printf("Grade: C\n");
-    else if (percentage >= 60)
+    }
+    else if (percentage >= 60) {
         printf("Grade: D\n");
-    else if (percentage >= 40)
+    }
+    else if (percentage >= 40) {
         printf("Grade: E\n");
-    else
+    }
+    else {
         printf("Grade: F\n");
+    }
 
     return 0;
 }
---
+++ ternary_operator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("enter the no for the check ");
+    scanf("%d",&a);
+    (a>=18)?printf("tu bada ho gaya he bacche "): printf("tu abhi bhi baccha ");
+   
+    return 0;
+    
+}
