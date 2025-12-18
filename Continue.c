8ba21efa9b51e89ce8d2018414e17fdcaa75ed4e
Aditya Orade <adityaraoorade@gmail.com>
2025-12-18 14:22:52 +0530

    Update Continue.c

--- Continue.c
+++ Continue.c
@@ -4,8 +4,8 @@ int main (){
     printf("\nExample of continue:\n");
     for (int i = 1; i <= 10; i++) {
         if (i == 5) {
-          //  printf("Skipping the value i = %d\n", i);
-            continue;  // Skip printing 5th, move to the next iteration
+          //printf("Skipping the value i = %d\n", i);
+            continue;  
         }
         printf("i = %d\n", i);
     }
